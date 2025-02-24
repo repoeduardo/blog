bb09a877320a0f3edc84e17cd35eb3bf08ebdff3
Eduardo Araujo Da Silva <eduardoaraujodasilva@protonmail.com>
2025-02-24 16:28:32 -0300

    Connection files and database table creation files were created based on Models

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# blog
+# Blog with FastAPI
 Simple blog project that uses FastAPI &amp; SQLite
--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,26 @@
+# fastapi imports
 from fastapi import FastAPI
+
+# database  imports
+from . import schemas, models
+from .dbconfig import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+# Creating the tables in the database based on the models in models.py file
+models.Base.metadata.create_all(engine)
 
 app = FastAPI()
 
+# connection with database
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# === GET ===
 
 @app.get('/')
 def index():
